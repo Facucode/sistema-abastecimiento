d6ff9f70948c7d3e153d728aaec47986e90a1f9f
Facucode <34733879+Facucode@users.noreply.github.com>
2021-02-03 23:14:21 -0300

    updating models details

--- app/models/elementofactura.model.js
+++ app/models/elementofactura.model.js
@@ -1,1 +1,28 @@
 /*Codigo Interno| Descripcion  | Cantidad| Bonific|Medida|Lista | abrev|Moneda| Pr.Lista…*/
+module.exports = (sequelize, DataTypes) => {
+    const Elementofactura = sequelize.define("elementofactura", {
+     codigo: {
+        type: DataTypes.INTEGER
+      },
+      descripcion: {
+        type: DataTypes.STRING
+      },
+      concepto: {
+        type: DataTypes.STRING
+      },
+      cantidad: {
+        type: DataTypes.FLOAT
+      },
+      medida: {
+        type: DataTypes.STRING
+      },
+      moneda:{type: DataTypes.STRING}
+      /*
+      fechalimite: {
+        type: DataTypes.DATEONLY
+
+      }*/
+    });
+  
+    return Elementofactura;
+  };
--- app/models/factura.model.js
+++ app/models/factura.model.js
@@ -3,3 +3,58 @@ Proveedor| % Ret IVA|pcia|
 Comprobante|prefijo|nro| nro CAI| vto CAI|
 L/Precio|C/Pago| moneda| deposito| fact[Manual O/compra/Rto]|%Fact|Vto |Concepto|Selección de articulos[por proveedor| todos]
 */
+module.exports = (sequelize, Sequelize) => {
+    const Factura = sequelize.define("factura", {
+      // Nuevo-Modificar-Duplicar??
+        division: {
+        type: Sequelize.STRING
+      },
+      prov: {
+        type: Sequelize.STRING
+      },
+     comprobante: {
+        type: Sequelize.INTEGER
+      },
+      prefijo:{type: Sequelize.STRING},
+      lprecio:{type: Sequelize.FLOAT},
+      cpago:{type: Sequelize.FLOAT},
+      moneda:{
+          type: Sequelize.INTEGER
+      },
+      concepto:{
+        type: Sequelize.STRING
+      },
+      numero:{
+        type: Sequelize.INTEGER
+      },
+      cai:{
+          type:Sequelize.INTEGER
+      },
+      vtocai:{
+        type: Sequelize.INTEGER
+      },    
+      fecha: {
+        type: Sequelize.DATEONLY
+      },
+      fechaddjj:{
+        type: Sequelize.DATEONLY
+      },
+      retiva:{
+        type: Sequelize.FLOAT
+      },
+      provincia:{
+        type: Sequelize.STRING
+      }
+
+      //LISTA DE PRECIOS?????
+
+      
+      //JOIN REMITO DE COMPRA (FECHA COMPROBANTE NUMERO PROVEEDOR DEPOSITO)
+      //JOIN PROVEEDORES
+      //ARTICULOS DEL PROVEEDOR / TODOS??
+
+
+    });
+  
+    return Tutorial;
+  };
